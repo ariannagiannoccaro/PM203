3ac3c2373b76223a7805918a6ef56d5791baa862
ariannagiannoccaro <123045858@upq.edu.mx>
2026-06-10 15:18:41 -0600

    Practica 6.1 - estructuras y componentes

--- Practica5/my-app-2/App.js
+++ Practica5/my-app-2/App.js
@@ -1,15 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// zona1: importaciones de componetes y archivos  
 
+import { StatusBar } from 'expo-status-bar'; //puede ser opcion quitarlo o no 
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {Saludo} from './components/Saludo';
+import {Saludo2} from './components/Saludo2';
+
+// zona2 main aquí van los componetes
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image></Image>
+      <Image source={require('./assets/wave.png')}/>
+      <Text>!------------------Componentes Nativos------------------</Text>
+      <Text>!Hola mundo React Native</Text>
+
+      <Text>!------------------Componentes Propios simples-----------</Text>
+      <Saludo></Saludo>
+      
+      
+      <Text>!------------------Componentes Propios Compuestos------</Text>
+      {/* <Saludo></Saludo> */}
+      <Saludo2></Saludo2>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
+// Zona 3 Estilos y Posicionamiento
 const styles = StyleSheet.create({
   container: {
     flex: 1,
