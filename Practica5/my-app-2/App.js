619c26d6b3ef97a21611a547dcbb049f956c2cb1
ariannagiannoccaro <123045858@upq.edu.mx>
2026-06-16 20:11:40 -0600

    Practica no.8 StyleSheets

--- Practica5/my-app-2/App.js
+++ Practica5/my-app-2/App.js
@@ -10,14 +10,14 @@ import { Perfil } from './components/Perfil';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Perfil
+      <Perfil estiloExt={styles.tarjetaRoja}
         nombre="Arianna"
         carrera="Ingenieria en Sistemas"
         materia="Programacion Movil"
         cuatri="9no">
 
       </Perfil>
-      <Perfil
+      <Perfil estiloExt={styles.tarjetaVerde}
         nombre="Jorge"
         carrera="Ingenieria en Sistemas"
         materia="BD"
@@ -25,6 +25,14 @@ export default function App() {
 
       </Perfil>
       <StatusBar style="auto" />
+      <Perfil estiloExt={styles.tarjetaRoja}
+        nombre="Arianna2"
+        carrera="Ingenieria en Sistemas"
+        materia="Programacion Movil"
+        cuatri="9no">
+
+      </Perfil>
+
     </View>
   );
 }
@@ -36,6 +44,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    flexDirection: 'row' 
+  }, 
+
+  tarjetaRoja: {
+    backgroundColor: '#FF6B6B',
   },
+
+  tarjetaVerde: {
+    backgroundColor: '#6BCB77',
+  }
 });
