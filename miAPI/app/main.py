a5e5e9d19409ad7ad3c72ac9cfc8d42e7c8038e9
ariannagiannoccaro <123045858@upq.edu.mx>
2026-07-28 19:55:50 -0600

    Practica 20: (conexion movil API) Select & insert

--- miAPI/app/main.py
+++ miAPI/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.routers import usuarios
 from app.data.db import engine
 from app.data import usuarioDB
+from fastapi.middleware.cors import CORSMiddleware
 
 #pertenece al funcionamiento del ORM de SQLAlchemy y sirve para 
 #crear automáticamente las tablas en la base de datos si aún no existen.
@@ -12,6 +13,21 @@ app = FastAPI(
     title="API usuarios ",
     description="Ivan Isay Guerra",
     version="1.0.0"
+)
+
+
+"""clientes con autorizacion a usar la API"""
+origins = [
+    "http://localhost:8081",
+    "http://127.0.0.1:8081" 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, #permite que los clientes de estos origenes se conecten a la API
+    allow_credentials=True, #permite que los clientes envien credenciales (cookies, autenticacion, etc.)
+    allow_methods=["*"], #significa que cualquier metodo HTTP esta permitido
+    allow_headers=["*"], #permite cualquier cabecera en la peticion
 )
 
 app.include_router(usuarios.router)
--- usuarioApi/screens/ConsultaUsuariosScreen.js
+++ usuarioApi/screens/ConsultaUsuariosScreen.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {SafeAreaView,View,Text,FlatList,StyleSheet,
 } from 'react-native';
 
 export default function ConsultaUsuariosScreen() {
 
-  const usuarios = [
-    { id: '1', nombre: 'Isay Guerra', edad: 22 },
-    { id: '2', nombre: 'Ana López', edad: 19 },
-    { id: '3', nombre: 'Carlos Gonzalez', edad: 25 },
-    { id: '4', nombre: 'Bjork Guerra', edad: 21 },
-    { id: '5', nombre: 'Luisa Martínez', edad: 28 },
-  ];
+  const [usuarios, setUsuario]= useState([]);
+
+  const obtenerUsuarios = async() =>{
+    try{
+      const respuesta = await fetch('http://localhost:5000/v1/usuarios/'); //poner la ip de mi pc para consultar desde el celular 
+      const datos = await respuesta.json();
+      console.log("Respuesta API: ", datos);
+      setUsuario(datos.usuarios);
+    }catch(error){
+      console.log("Error API: ", error);
+    }
+  };
+
+  useEffect(()=>{obtenerUsuarios();},[]);
 
   const renderTarjeta = ({ item }) => (
     <View style={styles.card}>
